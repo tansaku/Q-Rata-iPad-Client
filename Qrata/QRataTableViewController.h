e8f57c0e4c81b98210450920bf61d1c0e78b9e49
Sam Joseph <srjoseph@hawaii.edu>
2012-03-27 09:53:53 +0100

    adding basic qrata table view - not completely implemented yet

---
+++ Qrata/QRataTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRataTableViewController.h
+//  Qrata
+//
+//  Created by Samuel Joseph on 3/27/12.
+//  Copyright (c) 2012 NeuroGrid Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRataTableViewController : UITableViewController
+
+@end
